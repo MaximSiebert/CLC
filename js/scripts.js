75782c130ef0dba3f687c81d1aa81159ded8fc97
Maxim Siebert <maxim.siebert@gmail.com>
2017-06-29 15:11:11 -0400

    feat: fade animations

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   var easeOutQuad = function (x, t, b, c, d) {
       return -c *(t/=d)*(t-2) + b;
   }
+
   //Navigation
   $('a[href^="#"]').on('click', function(event) {
     var target = $( $(this).attr('href') );
@@ -35,6 +36,7 @@ $(document).ready(function(){
     }
   });
 
+  //Instagram Feed
   $.ajax({
       method: "GET",
       url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=328759248.2e06978.70cb8504405e43e6b6f6eb8a73cf3283'
@@ -69,5 +71,36 @@ $(document).ready(function(){
   	error: function(data){
   		console.log(data);
   	}
+  });
+
+  //On Scroll Animations
+  AOS.init({
+    offset: 300,
+  });
+
+  var a = 0;
+  $(window).scroll(function() {
+      $('.counter').each(function() {
+        var oTop = $(this).offset().top - window.innerHeight;
+        if ($(window).scrollTop() > oTop) {
+        var $this = $(this).find('.counter-value'),
+        countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        },
+        {
+          duration: 1000,
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+            complete: function() {
+            $this.text(this.countNum);
+          }
+        });
+      }
+      a = 1;
+    });
   });
 });
