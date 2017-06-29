5344f6663941100f933fdb2a533d4cb903c43bf1
Maxim Siebert <maxim.siebert@gmail.com>
2017-06-29 11:37:32 -0400

    fix: scroll bug

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
     var target = $( $(this).attr('href') );
     if( target.length ) {
         event.preventDefault();
-        window.location.hash = $(this).attr("href");
         $('html, body').animate({
             scrollTop: target.offset().top
         }, 1000, easeOutQuad);
+        window.location.hash = $(this).attr("href");
     }
   });
 
